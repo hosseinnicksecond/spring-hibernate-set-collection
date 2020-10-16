96978c2f706d2228bd9d11aff24b5ea0c3207acf
hossein nick <hossinnick62@gmail.com>
2020-10-16 15:32:21 +0330

    create StudentRepository and Startup.java

---
+++ src/main/java/home/train/bbotstrap/Startup.java
@@ -0,0 +1,12 @@
+package home.train.bbotstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Startup implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
